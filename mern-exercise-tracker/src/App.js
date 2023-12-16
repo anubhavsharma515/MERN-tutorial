98a6b3a6439bac6f805ff1bbdd8b10f02972a725
Anubhav Sharma <anubhav.sharma@thescore.com>
2023-12-15 20:05:20 -0500

    Add components

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <BrowserRouter>
+        <div className="container">
+        <Navbar />
+        <br/>
+        <Routes>
+          <Route path="/" exact element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
